7cfc56e4fbfb1e83ef2792af072a3a6d1472bf8c
P. Kerim Friedman <kerim.mail@oxus.net>
2017-08-29 10:48:56 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,9 @@ export default class Index extends React.Component {
           ]}
         />
         <h1>
-          Hello World!
+          This is a test of Gatsby!
         </h1>
-        <p>Welcome to your new clean Gatsby site</p>
+        <p>Consuetudium lectorum Mirum est notare. Eodem modo typi qui nunc nobis videntur parum clari fiant sollemnes in futurum? Assum Typi non habent claritatem insitam est usus legentis in iis. Claritatem Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius Claritas est etiam. Nam liber tempor cum soluta. Est etiam processus dynamicus qui.</p>
       </div>
     )
   }
